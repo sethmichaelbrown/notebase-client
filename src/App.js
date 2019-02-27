11cb10d4ab47dcd992279e23338b5796867fceac
SMB <sethmichaelbrown@gmail.com>
2019-02-27 13:53:00 -0700

    Basic implementation working

--- src/App.js
+++ src/App.js
@@ -4,27 +4,21 @@ import { Container } from 'react-bootstrap'
 
 
 import 'react-quill/dist/quill.snow.css'
-import './App.css';
+import './App.css'
 
+
+import TextEditor from './TextEditor/TextEditor'
 
 class App extends Component {
 
-  state = {
-    text: ''
-  }
-
-  handleChange(value) {
-    this.setState({ text: value })
-  }
 
   render() {
     return (
       <div className="App">
-        <div className="row container">
+        <h1>Hello from App.js</h1>
+        <div className="row">
           <div className="col-md-6">
-            <ReactQuill
-              value={this.state.text}
-              onChange={this.handleChange} />
+            <TextEditor />
           </div>
         </div>
       </div>
